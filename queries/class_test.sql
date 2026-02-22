6045852aabe229f292e0e28405f7a7773391fa1f
StellaMalakyan <stellamalakyan@gmail.com>
2026-02-22 13:14:19 +0400

    save the last changes

---
+++ queries/class_test.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS transactions_text_demo;
+
+CREATE TABLE transactions_text_demo (
+  transaction_id INTEGER,
+  customer_id    INTEGER,
+  raw_phone      TEXT,
+  category_raw   TEXT,
+  quantity       INTEGER,
+  price          NUMERIC(10,2)
+);
