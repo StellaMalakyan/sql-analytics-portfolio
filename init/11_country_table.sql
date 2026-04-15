0fa716f9177a8403c4e0ba476bce37b2e8c1d2bc
StellaMalakyan <stellamalakyan@gmail.com>
2026-04-15 16:29:28 +0400

    save inserting into country table

--- init/11_country_table.sql
+++ init/11_country_table.sql
@@ -6,3 +6,7 @@ CREATE TABLE job_analytics.country (
     geom GEOMETRY(MULTIPOLYGON, 4326)
 );
 
+-- 4th step
+INSERT INTO job_analytics.country (country_name, country_code, geom)
+SELECT country_name, country_code, geom
+FROM job_analytics._stg_world_countries;
