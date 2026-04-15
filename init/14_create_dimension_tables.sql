03bff65a233171e7f207552018561a1e9b85d56c
StellaMalakyan <stellamalakyan@gmail.com>
2026-04-15 23:17:02 +0400

    save populated dimension tables

--- init/14_create_dimension_tables.sql
+++ init/14_create_dimension_tables.sql
@@ -27,3 +27,38 @@ CREATE TABLE job_analytics.employment_type (
     employment_type_id SERIAL PRIMARY KEY,
     employment_type_name VARCHAR(50) UNIQUE NOT NULL
 );
+
+
+INSERT INTO job_analytics.employment_type (employment_type_name)
+VALUES ('Full-time'), ('Part-time'), ('Contract'), ('Freelance')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO job_analytics.remote_work_type (remote_work_name)
+SELECT DISTINCT TRIM(remote_work) 
+FROM job_analytics._stg_salary_prediction 
+WHERE remote_work IS NOT NULL 
+ON CONFLICT DO NOTHING;
+
+INSERT INTO job_analytics.education_level (education_name)
+SELECT DISTINCT TRIM(education_level) 
+FROM job_analytics._stg_salary_prediction 
+WHERE education_level IS NOT NULL 
+ON CONFLICT DO NOTHING;
+
+INSERT INTO job_analytics.job_title (job_title_name)
+SELECT DISTINCT TRIM(job_title) 
+FROM job_analytics._stg_salary_prediction 
+WHERE job_title IS NOT NULL 
+ON CONFLICT DO NOTHING;
+
+INSERT INTO job_analytics.industry (industry_name)
+SELECT DISTINCT TRIM(industry) 
+FROM job_analytics._stg_salary_prediction 
+WHERE industry IS NOT NULL 
+ON CONFLICT DO NOTHING;
+
+INSERT INTO job_analytics.company_size (size_category)
+SELECT DISTINCT TRIM(company_size) 
+FROM job_analytics._stg_salary_prediction 
+WHERE company_size IS NOT NULL 
+ON CONFLICT DO NOTHING;
--- init/15_create_salary_fact_table.sql
+++ init/15_create_salary_fact_table.sql
@@ -1,3 +1,20 @@
+CREATE TABLE job_analytics.salary_fact (
+    fact_id SERIAL PRIMARY KEY,
+    job_title_id INT REFERENCES job_analytics.job_title(job_title_id),
+    industry_id INT REFERENCES job_analytics.industry(industry_id),
+    education_id INT REFERENCES job_analytics.education_level(education_id),
+    company_size_id INT REFERENCES job_analytics.company_size(company_size_id),
+    remote_work_id INT REFERENCES job_analytics.remote_work_type(remote_work_id),
+    employment_type_id INT REFERENCES job_analytics.employment_type(employment_type_id),
+    city_id INT REFERENCES job_analytics.city(city_id),
+    
+    experience_years NUMERIC,
+    salary NUMERIC,
+    certificates_count INT,
+    skills_count INT,
+    job_post_date DATE
+);
+
 INSERT INTO job_analytics.salary_fact (
     job_title_id, 
     industry_id, 
